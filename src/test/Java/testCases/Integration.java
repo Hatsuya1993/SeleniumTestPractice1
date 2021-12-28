7a861aa2b2d14841fa09e0822957ab5e7b172759
Hazrul <hal1993@hotmail.com>
2021-12-29 07:07:47 +0800

    New Test

--- src/test/Java/testCases/Integration.java
+++ src/test/Java/testCases/Integration.java
@@ -223,4 +223,22 @@ public class Integration extends Base{
         }
     }
 
+    @Test
+    public void checkTheLinkForHotelstonDoc() throws InterruptedException {
+        DemoPagePOM demoPagePOM = new DemoPagePOM(driver);
+        Thread.sleep(helper.shortWait);
+        available.clickItem(demoPagePOM.getIntegrationLink());
+        IntegrationPagePOM integrationPagePOM = new IntegrationPagePOM(driver);
+        available.clickItem(integrationPagePOM.getHotelstonDoc());
+        available.switchTab(1, driver);
+        if(driver.getCurrentUrl().contains("hotels/hotelston")){
+            Assert.assertTrue(true);
+            logger.info("Url for hotelston doc is correct");
+        }
+        else{
+            Assert.fail();
+            logger.info("Url for the hotelston doc is incorrect");
+        }
+    }
+
 }
