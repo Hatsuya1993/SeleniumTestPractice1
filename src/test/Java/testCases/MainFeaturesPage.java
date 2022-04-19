b64b60b8a5612ffe13c5ade05d024324ad991cef
Hazrul <hal1993@hotmail.com>
2022-04-19 09:46:44 +0800

    New Test

--- src/test/Java/testCases/MainFeaturesPage.java
+++ src/test/Java/testCases/MainFeaturesPage.java
@@ -414,4 +414,18 @@ public class MainFeaturesPage extends Base {
                     "populated");
         }
     }
+
+    @Test
+    public void securityOptionsEighthCheck() throws InterruptedException {
+        MainFeaturesPagePOM mainFeaturesPagePOM =
+                new MainFeaturesPagePOM(driver);
+        Actions.scrollHelperImplicit(500, driver);
+        if (Helper.displayed(mainFeaturesPagePOM.getSecurityOptionsSelectorHelper(6))) {
+            Assert.assertTrue(true);
+            logger.info("Security options selector eighth title displayed");
+        } else {
+            Assert.fail();
+            logger.info("Security options eighth title is not displayed");
+        }
+    }
 }
