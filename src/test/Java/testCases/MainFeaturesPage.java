7c4405c2d4e6d90b974e986148f420c1783e765f
Hazrul <hal1993@hotmail.com>
2022-04-25 08:45:28 +0800

    New Test

--- src/test/Java/testCases/MainFeaturesPage.java
+++ src/test/Java/testCases/MainFeaturesPage.java
@@ -504,4 +504,18 @@ public class MainFeaturesPage extends Base {
                     "populated");
         }
     }
+
+    @Test
+    public void securityOptionsEleventhCheck() throws InterruptedException {
+        MainFeaturesPagePOM mainFeaturesPagePOM =
+                new MainFeaturesPagePOM(driver);
+        Actions.scrollHelperImplicit(500, driver);
+        if (Helper.displayed(mainFeaturesPagePOM.getSecurityOptionsSelectorHelper(9))) {
+            Assert.assertTrue(true);
+            logger.info("Security options selector Eleventh title displayed");
+        } else {
+            Assert.fail();
+            logger.info("Security options Eleventh title is not displayed");
+        }
+    }
 }
