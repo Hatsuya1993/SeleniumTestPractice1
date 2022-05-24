5a3c93f67f596be8a28ee969ebce86d21dccedc1
Hazrul <hal1993@hotmail.com>
2022-05-24 08:40:13 +0800

    New Test

--- src/test/Java/pageObjects/HotelsModuleFeaturesPagePOM.java
+++ src/test/Java/pageObjects/HotelsModuleFeaturesPagePOM.java
@@ -1,4 +1,24 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import utilities.ReadConfig;
+
 public class HotelsModuleFeaturesPagePOM {
+
+    ReadConfig readConfig = new ReadConfig();
+
+    public String hotelsModuleFeatures = readConfig.getIntegrationURL();
+
+    WebDriver driver;
+    public HotelsModuleFeaturesPagePOM(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+        this.driver.get(hotelsModuleFeatures);
+    }
+
+
+
+
+
 }
--- src/test/Java/testCases/HotelsModuleFeaturesPage.java
+++ src/test/Java/testCases/HotelsModuleFeaturesPage.java
@@ -1,4 +1,94 @@
 package testCases;
 
-public class HotelsModuleFeaturesPage {
+public class HotelsModuleFeaturesPage extends Base{
+
+    // Check the url is correct
+
+    // Check the title is displayed
+
+    // Check the title is displayed correctly
+
+    // Check the title description is displayed
+
+    // Check the title description is displayed correctly
+
+    // Check the mod 1 title is displayed
+
+    // Check the mod 1 title is  displayed correctly
+
+    // Check the mod 1 description is displayed
+
+    // Check the mod 1 description is displayed correctly
+
+    // Check the mod 1 image is displayed
+
+    // Check the mod 2 title is displayed
+
+    // Check the mod 2 title is  displayed correctly
+
+    // Check the mod 2 description is displayed
+
+    // Check the mod 2 description is displayed correctly
+
+    // Check the mod 2 image is displayed
+
+    // Check  the main features title is displayed
+
+    // Check the main features title is displayed correctly
+
+    //Check the number of features tally
+
+    // Check each feature image is displayed
+
+    // Check each feature title is displayed
+
+    // Check each feature title is displayed correctly
+
+    // Check each feature description is displayed
+
+    // Check each feature description is displayed correctly
+
+    // Check module flow title is displayed
+
+    // Check module flow title is displayed correctly
+
+    // Check module flow description is displayed
+
+    // Check module flow description is displayed correctly
+
+    //Check overview button is displayed
+
+    // Check overview button text is displayed correctly
+
+    // Check click of button move to the demo page by url
+
+    // Check module flow image is displayed
+
+    // Check google map title is displayed
+
+    // Check google map title is displayed correctly
+
+    // Check google map description is displayed
+
+    // Check google map description is displayed correctly
+
+    // Check google map image is displayed
+
+    // Check hotel manage title is displayed
+
+    // Check hotel manage title is displayed correctly
+
+    // Check hotel manage description is displayed
+
+    // Check hotel manage description is displayed correctly
+
+    // Check hotel manage button is displayed
+
+    // Check hotel button text is displayed correctly
+
+    // Check click button move to order page via url
+
+    // Check regions is displayed
+
+
 }
